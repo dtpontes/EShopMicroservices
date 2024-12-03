ff50762f1d8f32a3a3ad308be715d4ae011db5e6
Daniel Teixeira Pontes <daniel.pontes@xavipay.com>
2024-12-03 13:38:07 -0300

    Modelagem do Dominio

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models;
+
+public class OrderItem: Entity<Guid>
+{
+    public OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+    {
+        ProductId = productId;
+        OrderId = orderId;
+        Quantity = quantity;
+        Price = price;
+    }
+
+
+    public Guid ProductId { get; private set; } = default!;
+
+    public Guid OrderId { get; private set; } = default!;
+
+    public int Quantity { get; private set; } = default!;
+
+    public decimal Price { get; private set; } = default!;
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string CardName { get; } = default!;
+
+    public string CardNumber { get; } = default!;    
+
+    public DateTime Expiration { get; } = default!;
+
+    public string CVV { get; } = default!;
+
+    public int PaymentMethod { get; } = default!;
+
+
+
+}
